6e6806a72161a22c4cbdf7cba3078683b0e96ecd
Paul Annesley <paul@annesley.cc>
2019-09-05 14:40:26 +1000

    secure middleware for authentication

--- main.go
+++ main.go
@@ -18,12 +18,24 @@ func main() {
 		io.WriteString(w, "hello world")
 	})
 
-	http.ListenAndServe(":"+port, httplog(app))
+	http.ListenAndServe(":"+port, httplog(secure(app)))
 }
 
 func httplog(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Println(r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
+}
+
+// secure verifies that all requests have an extremely strong password
+func secure(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// auth the request
+		if r.Header.Get("Authorization") != "hunter2" {
+			w.WriteHeader(401)
+			return
+		}
 		next.ServeHTTP(w, r)
 	})
 }
